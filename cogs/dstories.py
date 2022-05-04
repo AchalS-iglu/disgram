c195c250a759fe1925a080613f4685a798253a08
Achal Singhal <achals2002@gmail.com>
2022-05-05 00:30:55 +0530

    updatechannel implemented

--- cogs/dstories.py
+++ cogs/dstories.py
@@ -4,7 +4,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import json, os
+import datetime
 
 class DStories(commands.Cog, name="dstories"):
     def __init__(self, bot: commands.Bot) -> None:
@@ -18,6 +18,7 @@ class DStories(commands.Cog, name="dstories"):
     
     @group.command(name="adduser")
     async def adduser(self, interaction: discord.Interaction, username: str, channel: discord.TextChannel) -> None:
+        await interaction.response.defer(ephemeral=False)
         user_id = self.bot.igclient.user_id_from_username(username)
         guild_id = interaction.guild_id
         
@@ -38,18 +39,46 @@ class DStories(commands.Cog, name="dstories"):
             self.bot.cur.execute(f"""INSERT INTO "{guild_id}" ("user", "channel")
                                  VALUES ('{user_id}', '{channel.id}')
                                  """)
+            self.bot.conn.commit()
+            self.bot.dblog.write(f'[{datetime.now.strftime("%d/%m/%Y %H:%M:%S")}]: ADDUSER: User {user_id} has been added to {guild_id} with channel set as {channel.id}.\n')
+            await interaction.response.send_message(f"User has {username} been added to tracking list.")
         else:
             self.bot.cur.execute(f"""UPDATE "{guild_id}"
                                  SET "channel" = '{channel.id}'
                                  WHERE "user" = '{user_id}'
                                  """)
-        self.bot.conn.commit()
-        self.bot.dblog.write(f'ADDUSER: User {user_id} has been added to {guild_id} with channel set as {channel.id}.')
-        await interaction.response.send_message(f"User has {username} been added to tracking list.")
+            self.bot.conn.commit()
+            self.bot.dblog.write(f'[{datetime.now.strftime("%d/%m/%Y %H:%M:%S")}]: ADDUSER: User {user_id} of guild {guild_id} channel set to {channel.id}.\n')
+            await interaction.response.send_message(f"User {username} was already in database although the channel has been updated")
+
     
-    @group.command(name='setchannel')
-    async def setchannel(self, interaction: discord.Interaction, user: str, channel: discord.TextChannel):
-        pass
+    @group.command(name='updatechannel')
+    async def setchannel(self, interaction: discord.Interaction, username: str, channel: discord.TextChannel):
+        await interaction.response.defer(ephemeral=False)
+        user_id = self.bot.igclient.user_id_from_username(username)
+        guild_id = interaction.guild_id
+                
+        self.bot.cur.execute(f"""SELECT "user", "channel"
+                                       FROM "{guild_id}"
+                                       WHERE "user" = '{user_id}'
+                                       """)     
+        output = self.bot.cur.fetchall()
+
+        if len(output) == 0:
+            self.bot.cur.execute(f"""INSERT INTO "{guild_id}" ("user", "channel")
+                                 VALUES ('{user_id}', '{channel.id}')
+                                 """)
+            self.bot.conn.commit()
+            self.bot.dblog.write(f'[{datetime.now.strftime("%d/%m/%Y %H:%M:%S")}]: ADDUSER: User {user_id} has been added to {guild_id} with channel set as {channel.id}.\n')
+            await interaction.response.send_message(f"User {username} was not present in database but has been added.")
+        else:
+            self.bot.cur.execute(f"""UPDATE "{guild_id}"
+                                 SET "channel" = '{channel.id}'
+                                 WHERE "user" = '{user_id}'
+                                 """)
+            self.bot.conn.commit()
+            self.bot.dblog.write(f'[{datetime.now.strftime("%d/%m/%Y %H:%M:%S")}]: ADDUSER: User {user_id} of guild {guild_id} channel set to {channel.id}.\n')
+            await interaction.response.send_message(f"User {username}'s channel has been updated")
         
 
 async def setup(bot: commands.Bot) -> None:
